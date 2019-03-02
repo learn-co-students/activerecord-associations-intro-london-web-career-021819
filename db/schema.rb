054f016a54124b7800826f82d9205d6d46f55679
github username <github email address>
2019-03-02 17:04:09 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name"
+    t.integer "name"
   end
 
   create_table "songs", force: :cascade do |t|
